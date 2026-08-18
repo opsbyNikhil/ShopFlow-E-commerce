3f38dcb3499a2b91fdb2234de37fe82d1a46a2c8
opsbyNikhil <purposejob97@gmail.com>
2026-08-18 13:32:13 +0530

    changed entrypint.sh

--- auth-service/entrypoint.sh
+++ auth-service/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for database..."
-until python manage.py shell -c "from django.db import connection; connection.ensure_connection()" 2>/dev/null; do
-  echo "Database unavailable, waiting 2s..."
-  sleep 2
-done
-echo "Database is up."
+echo "Running databsae migrations....."
+python manage.py migrate --noinput
 
 echo "Running database migrations....."
 python manage.py migrate --noinput
