cf5db6f8e7792e1c6a221461b339d90d1358fc51
opsbyNikhil <purposejob97@gmail.com>
2026-08-18 16:39:15 +0530

    fix auth migration chain

--- auth-service/accounts/migrations/0004_alter_pendingregistration_otp.py
+++ auth-service/accounts/migrations/0004_alter_pendingregistration_otp.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0004_alter_pendingregistration_otp'),
+        ('accounts', '0003_loginsession'),
     ]
 
     operations = [
